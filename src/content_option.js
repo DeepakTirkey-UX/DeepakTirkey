d8c24bb3c27225b05376e4e8b29edcc35989232e
Deepak Tirkey <deepaktirkey341@gmail.com>
2024-09-04 00:55:25 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "DEEPAK";
+const meta = {
+    title: "DEEPAK TIRKEY",
+    description: "I’m DEEPAK TIRKEY data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m DEEPAK TIRKEY",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am a positive person in every aspect of life, full of enthusiasm and wonder. There are so many things I love to do, see, and experience. With a childlike spirit, I enjoy gazing at the clouds with a clear mind and embracing the beauty of nature. My village, nestled in a small corner of the Sundergarh district, feels like a world of its own. It’s where my journey began on 20 May 1955, and where the memories of my childhood are deeply rooted. Growing up, I cherished playing with friends, exploring the surroundings, and immersing myself in the simple joys of life. These moments marked the beginning of my childhood, shaping who I am today. Throughout my lifetime, I’ve had the pleasure of meeting and introducing myself to hundreds of new people, each interaction adding to the richness of my experiences.",
+    your_img_url: "https://i.pinimg.com/550x/eb/3e/59/eb3e5975427b9a486984b58d1173f050.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a positive person in every aspect of life, full of enthusiasm and wonder. There are so many things I love to do, see, and experience. With a childlike spirit, I enjoy gazing at the clouds with a clear mind and embracing the beauty of nature. My village, nestled in a small corner of the Sundergarh district, feels like a world of its own. It’s where my journey began on 20 May 1955, and where the memories of my childhood are deeply rooted. Growing up, I cherished playing with friends, exploring the surroundings, and immersing myself in the simple joys of life. These moments marked the beginning of my childhood, shaping who I am today. Throughout my lifetime, I’ve had the pleasure of meeting and introducing myself to hundreds of new people, each interaction adding to the richness of my experiences. Growing up, I cherished playing with friends, exploring the surroundings, and immersing myself in the simple joys of life. These moments marked the beginning of my childhood, shaping who I am today. Throughout my lifetime, I’ve had the pleasure of meeting and introducing myself to hundreds of new people, each interaction adding to the richness of my experiences.",
+};
+const worktimeline = [{
+        jobtitle: "Designer Graphice",
+        where: "Adobe Photoshop, Adobe Illustrator, Premiere Pro, After Effects, Adobe Flash, CorelDraw & Figma ",
+        date: "2013 - 2024",
+    },
+    {
+        jobtitle: "Web Designer",
+        where: "HTML5 & CSS3, AMP, Bootstrap, Shopify",
+        date: "2016 - 2024",
+    },
+    {
+        jobtitle: "Web Development",
+        where: "WordPress (CMS), MySQL",
+        date: "2020 - 2024",
+    },
+];
+
+const skills = [{
+        name: "WordPress (CMS)",
+        value: 90,
+    },
+    {
+        name: "HTML5 & CSS3",
+        value: 95,
+    },
+    {
+        name: "Photoshop",
+        value: 99,
+    },
+    {
+        name: "Illustrator",
+        value: 80,
+    },
+    {
+        name: "Premiere Pro",
+        value: 85,
+    },
+    {
+        name: "After Effects",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I am a positive person in every aspect of life, full of enthusiasm and wonder. There are so many things I love to do, see, and experience. With a childlike spirit, I enjoy gazing at the clouds with a clear mind and embracing the beauty of nature. My village, nestled in a small corner of the Sundergarh district, feels like a world of its own. It’s where my journey began on 20 May 1955, and where the memories of my childhood are deeply rooted. sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "I am a positive person in every aspect of life, full of enthusiasm and wonder. There are so many things I love to do, see, and experience. With a childlike spirit, I enjoy gazing at the clouds with a clear mind and embracing the beauty of nature. My village, nestled in a small corner of the Sundergarh district, feels like a world of its own. It’s where my journey began on 20 May 1955, and where the memories of my childhood are deeply rooted. sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "I am a positive person in every aspect of life, full of enthusiasm and wonder. There are so many things I love to do, see, and experience. With a childlike spirit, I enjoy gazing at the clouds with a clear mind and embracing the beauty of nature. My village, nestled in a small corner of the Sundergarh district, feels like a world of its own. It’s where my journey began on 20 May 1955, and where the memories of my childhood are deeply rooted. sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/53a24e155268671.63513134ec357.jpg",
+        description: "L&P Global Security",
+        link: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/53a24e155268671.63513134ec357.jpg",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e056b1155268539.635130b80e12c.png",
+        description: "Paticia Fernets",
+        link: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e056b1155268539.635130b80e12c.png",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7bed9b155268433.6351306927186.jpg",
+        description: "The Photo Team",
+        link: "https://www.behance.net/gallery/155268433/Photography-Web-Template/modules/876070719",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/878857155268245.63512fd143aa3.jpg",
+        description: "Half Moon Bay Restaurant",
+        link: "https://www.behance.net/gallery/155268245/Restaurant-Web-Template",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/551890153847615.6336ee7f47395.jpg",
+        description: "Nancy Land Kids And baby",
+        link: "https://www.behance.net/gallery/153847615/Web-Sample-10",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/3cd928153847137.6336ed1a2ecbc.jpg",
+        description: "QGPU CHEM",
+        link: "https://www.behance.net/gallery/153847137/Web-Sample-6",
+    },
+
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/206e2e153847403.6336edd7e4e1d.jpg",
+        description: "HDrive ",
+        link: "https://www.behance.net/gallery/153847403/Web-Sample-8",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/06714e153847279.63622a6399647.jpg",
+        description: "Thrive Martial Arts",
+        link: "https://www.behance.net/gallery/153847279/Web-Sample-7",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/886619153846261.6336e9c900d6d.jpg",
+        description: "Pacifica Chamber of Commerce & Visitor Center",
+        link: "https://www.behance.net/gallery/153846261/Web-Sample-1",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/64059c153846671.6336eb46d0baa.jpg",
+        description: "Le Createur",
+        link: "https://www.behance.net/gallery/153846671/Web-Sample-2",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f8c10e153848077.6336efed635f2.jpg",
+        description: "Video 360 Booth",
+        link: "https://www.behance.net/gallery/153848077/Web-Sample-11",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7e8cd4153847521.6336ee16565b1.jpg",
+        description: "Donna ever More",
+        link: "https://www.behance.net/gallery/153847521/Web-Sample-9",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "deepaktirkey@gmail.com",
+    YOUR_FONE: "+91 969 2554 512",
+    description: "Growing up, I cherished playing with friends, exploring the surroundings, and immersing myself in the simple joys of life. These moments marked the beginning of my childhood, shaping who I am today. Throughout my lifetime, I’ve had the pleasure of meeting and introducing myself to hundreds of new people, each interaction adding to the richness of my experiences.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/DeepakTirkey-UX",
+    facebook: "https://www.facebook.com/DeepakTirkey1995",
+    linkedin: "https://www.linkedin.com/in/deepaktirkey341",
+    twitter: "https://twitter.com/deepaktirkey341/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
